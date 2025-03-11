e2f47fe09eac5718034bd55b81e6376b281a1091
焦少康 <2524385023@qq.com>
2025-03-11 20:53:36 +0800

    基础设施搭建

---
+++ services/gateway-service/src/main/java/com/learn/index12306/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.learn.index12306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: cyy
+ * @create: 2025-03-11 19:20
+ **/
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
