a5e847b9be3fd8ba27ab82b10e2160eb60ea36f6
焦少康 <2524385023@qq.com>
2025-03-15 16:24:35 +0800

    日志模块搭建

---
+++ frameworks/log/src/main/java/com/learn/index12306/framework/starter/log/annotation/ILog.java
@@ -0,0 +1,28 @@
+package com.learn.index12306.framework.starter.log.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author: cyy
+ * @create: 2025-03-15 15:32
+ * @description: Log 注解打印，可以标记在类或者方法上
+ **/
+@Target({ElementType.TYPE, ElementType.METHOD}) // 指定注解作用在类上或者方法上
+@Retention(RetentionPolicy.RUNTIME) // 指定注解在运行时有效
+public @interface  ILog {
+
+    /**
+     * 入参打印
+     * @return 打印结果中是否包含入参，{@link Boolean#TRUE} 打印，{@link Boolean#FALSE} 不打印
+     */
+    boolean input() default true;
+
+    /**
+     * 出参打印
+     * @return 打印结果中是否包含出参，{@link Boolean#TRUE} 打印，{@link Boolean#FALSE} 不打印
+     */
+    boolean output() default true;
+}
---
+++ frameworks/log/src/main/java/com/learn/index12306/framework/starter/log/config/LogAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.learn.index12306.framework.starter.log.config;
+
+import com.learn.index12306.framework.starter.log.core.ILogPrintAspect;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author: cyy
+ * @create: 2025-03-15 16:13
+ * @description: 日志自动装配类
+ **/
+public class LogAutoConfiguration {
+
+    @Bean
+    public ILogPrintAspect iLogPrintAspect() {
+        return new ILogPrintAspect();
+    }
+}
